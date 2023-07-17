9ef37baa9105dda619002fd8cd1877805892abc6
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2023-07-18 04:45:01 +0800

    feat: incl location route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import apiCall from "../service/apiCall";
+import locationCall from "../service/locationCall";
 
 const router = express.Router();
 
 // Fetch restaurants
 router.post("/search", apiCall);
+
+// Fetch location
+router.post("/location", locationCall);
 
 export default router;
